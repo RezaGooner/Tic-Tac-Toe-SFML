b0a573dcc30728645769748d4605ae48a0661d81
𝐑𝐞𝐳𝐚 𝐀𝐬𝐚𝐝𝐢 <149728623+RezaGooner@users.noreply.github.com>
2025-01-24 18:35:57 +0330

    Add files via upload

---
+++ 3x3/cmake-build-debug/CMakeFiles/SFMLProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFMLProject.dir/main.cpp.obj"
+  "CMakeFiles/SFMLProject.dir/main.cpp.obj.d"
+  "SFMLProject.exe"
+  "SFMLProject.exe.manifest"
+  "SFMLProject.pdb"
+  "libSFMLProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFMLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
